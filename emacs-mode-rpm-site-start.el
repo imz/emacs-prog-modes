a64e26e2f4c3ced075e67a463317ea5951d2cea9
Ivan Zakharyaschev <imz@altlinux.org>
2015-12-13 23:51:51 +0300

    added an example how to turn off the automatic filling

--- emacs-mode-rpm-site-start.el
+++ emacs-mode-rpm-site-start.el
@@ -7,3 +7,5 @@
 (autoload 'rpm-spec-mode "rpm-spec-mode" "RPM spec mode." t)
 (setq auto-mode-alist (append '(("\\.spec" . rpm-spec-mode))
 			      auto-mode-alist))
+;; If you do not want to automatically fill new empty .spec-files:
+;; (setq rpm-spec-initialize-sections nil)
